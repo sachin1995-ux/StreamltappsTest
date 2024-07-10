7427c89e00eec4324cf235a3b1a2f7a22d870d31
sachin_dev_v1 <146861767+sachin1995-ux@users.noreply.github.com>
2024-07-10 06:16:19 +0530

    Update SS_TestPackage_dashboard.py

--- data/SS_TestPackage_dashboard.py
+++ data/SS_TestPackage_dashboard.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+# import plotly.express as px
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
